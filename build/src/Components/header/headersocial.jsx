573352f707e832ea3a00e4ccca4f9612b0167eff
HariSankar3199 <harisankarvalath1@gmail.com>
2023-03-08 08:33:15 +0530

    handling error

--- build/src/Components/header/headersocial.jsx
+++ build/src/Components/header/headersocial.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './header.css'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
 const Headersocial = () => {
   return (
     <div className='header__socials'>
-<a href="https://linkedin.com/in/hari-sankar-77504b222" target='_blank'><BsLinkedin/></a>
-<a href="https://github.com/HariSankar3199"  target='_blank'><FaGithub/></a>
+      <a href="https://www.linkedin.com/in/hari-sankar-77504b222/" target='_blank'><BsLinkedin /></a>
+      <a href="https://www.github.com/HariSankar3199" target='_blank'><FaGithub /></a>
 
     </div>
   )
